62110419ddee079d37a9ce8705ca275824b60007
Ian-Repik <irepik@email.sc.edu>
2026-09-13 16:47:36 -0700

    update commit

--- csce-242/assignments/05java/intro.js
+++ csce-242/assignments/05java/intro.js
@@ -1,13 +1,4 @@
-
-document.getElementById("btn-show-message").onclick = (e) =>{
-    document.getElementById("p-message").innerHTML="Hello";
-    e.target.innerHTML = "done!";
-}
-document.getElementById("link").onclick = (e) =>{
-    e.preventDefault();
-    e.target.classList.add ("cool-link" );
-}
-document.getElementById("ball").onclick = (e) =>{
-    e.preventDefault();
-    e.target.classList.toggle ("bouncing-ball" );
+document.getElementById("click").onclick = (e) => {
+  document.getElementById("p-message").innerHTML = "Hello";
+  e.target.innerHTML = "done!";
 }
